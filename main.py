708fb50ad3e7d57c592fcd7a6476d6246c8dac54
doktorbr4in <doktorbr4in@gmail.com>
2021-01-08 08:15:22 +0100

    added functions

--- main.py
+++ main.py
@@ -1,32 +1,125 @@
+import random
 
-class Map:
+class Character:
+    def __init__(self,hp,ad,name):
+        self.hp = hp 
+        self.ad = ad 
+        self.name = name
+
+    def get_hit(self,ad):
+        self.hp = self.hp - ad
+        if self.hp <= 0:
+            self.die()
+
+    def ist_dead(self):
+        return self.hp <= 0
+
+    def die(self):
+        print(self.name + " died")
+
+class Goblin(Character):
     def __init__(self):
-        pass
+        Character.__init__(self,100,10,"Goblin")
+
+class Ork(Character):
+    def __init__(self):
+        Character.__init__(self,300,30,"Ork")
 
 class Player:
-    def __init__(self,name):
-        self.name = name
+    def __init__(self,name,hp ,ad):
+        Character.__init__(self,hp,ad,name)
+        self.max_hp = hp
+
+    def die(self):
+        exit("Wasted. Try again.")
+
+    def rest(self):
+        self.hp=self.max_hp
+
+class Field:
+    def __init__(self,enemies):
+        self.enemies = enemies
+        self.loot=[]
+
+    def print_state(self):
+        print("You look around and see ")
+        for i in self.enemies:
+            print(i.name)
+
+    @staticmethod
+    def gen_random():
+        rand=random.randint(0,2)
+        if rand == 0:
+            return Field([])
+        if rand == 1:
+            return Field([Ork()])
+        if rand == 2:
+            return Field([Goblin(),Goblin(),Ork()])
+
+class Map:
+    def __init__(self,width,height):
+        self.state=[]
+        self.x=0
+        self.y=0
+        for i in range(width):
+            fields=[]
+            for j in range(height):
+                fields.append(Field.gen_random())
+            self.state.append(fields)
+
+    def print_state(self):
+        self.state[self.x][self.y].print_state
+
+    def get_enemies(self):
+        self.state[self.x][self.y].enemies
+
+    def forward(self):
+        if self.x == len(self.state) - 1:
+            print("You see hughe mountains, which you can't pass.")
+        else:
+            self.x = self.x + 1
+            
+    def backwards(self):
+        if self.x == 0:
+            print("You see cliffs, which you can't pass.")
+        else:
+            self.x = self.x - 1
+
+    def right(self):
+        if self.y == len(self.state[self.x]) - 1:
+            print("You see hughe mountains, which you can't pass.")
+        else:
+            self.y = self.y + 1
+            
+    def left(self):
+        if self.y == 0:
+            print("You see cliffs, which you can't pass.")
+        else:
+            self.y = self.y - 1
+
+
 
 def print_help(p,m):
     print(Commands.keys())
 
 def quit_game(p,m):
+    print("You decide to leave... Bye")
     exit(0)
 
 def pickup(p,m):
     pass
 
 def forward(p,m):
-    pass
+    m.forward()
 
 def right(p,m):
-    pass
+    m.right()
 
 def left(p,m):
-    pass
+    m.left()
 
 def backwards(p,m):
-    pass
+    m.backwards()
 
 def fight(p,m):
     pass
@@ -38,7 +131,7 @@ def load(p,m):
     pass
 
 def rest(p,m):
-    pass
+    p.rest()
 
 Commands = {
     'help' : print_help,
@@ -58,8 +151,8 @@ if  __name__ =='__main__':
     print("*"*20 + "Textadventure" + "*"*20)
     name = input("Enter your name: ")
 
-    p = Player(name)
-    mmap = Map()
+    p = Player(name,200,20)
+    mmap = Map(5,5)
     print("(type help to list all available commands)\n")
 
     while True:
@@ -68,3 +161,4 @@ if  __name__ =='__main__':
             Commands[command[0]](p, mmap)
         else:
             print("You run around in circles and don't know what to do...")
+            mmap.print_state()
